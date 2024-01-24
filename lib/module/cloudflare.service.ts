76ce8935a4a43dc5d253a522ddbccfa1899f68f0
brtkkkkk <121765793+brtkkkkk@users.noreply.github.com>
2024-01-25 02:42:08 +0300

    Update

--- lib/module/cloudflare.service.ts
+++ lib/module/cloudflare.service.ts
@@ -82,4 +82,139 @@ export class CloudflareService {
       }
     };
   }
+
+  //Burası
+
+  getZonesByZoneIdentifierCertificateAuthoritiesHostnameAssociations = async ({ zone_identifier, mtls_certificate_id }: IGetZonesByZoneIdentifierCertificateAuthoritiesHostnameAssociationsParams): Promise<IGetZonesByZoneIdentifierCertificateAuthoritiesHostnameAssociationsResponse> => {
+    this.httpService.get(this.url + `/zones/${zone_identifier}/certificate_authorities/hostname_associations`)
+    return true;
+  }
+
+  putZonesByZoneIdentifierCertificateAuthoritiesHostnameAssociations = async ({ zone_identifier, hostnames, mtls_certificate_id }: IPutZonesByZoneIdentifierCertificateAuthoritiesHostnameAssociationsParams): Promise<IPutZonesByZoneIdentifierCertificateAuthoritiesHostnameAssociationsResponse> => {
+    this.httpService.put(this.url + `/zones/${zone_identifier}/certificate_authorities/hostname_associations`)
+    return true;
+  }
+
+  getZonesByZoneIdentifierClientCertificates = async ({ zone_identifier, limit, offset, page, per_page, status }: IGetZonesByZoneIdentifierClientCertificatesParams): Promise<IGetZonesByZoneIdentifierClientCertificatesResponse> => {
+    this.httpService.get(this.url + `/zones/${zone_identifier}/client_certificates`)
+    return true;
+  }
+
+  postZonesByZoneIdentifierClientCertificates = async ({ zone_identifier, csr, validity_days }: IPostZonesByZoneIdentifierClientCertificatesParams): Promise<IPostZonesByZoneIdentifierClientCertificatesResponse> => {
+    this.httpService.post(this.url + `/zones/${zone_identifier}/client_certificates`)
+    return true;
+  }
+
+  deleteZonesByZoneIdentifierClientCertificatesIdentifier = async ({ client_certificate_identifier, zone_identifier }: IDeleteZonesByZoneIdentifierClientCertificatesIdentifierParams): Promise<IDeleteZonesByZoneIdentifierClientCertificatesIdentifierResponse> => {
+    this.httpService.delete(this.url + `/zones/${zone_identifier}/client_certificates/${client_certificate_identifier}`)
+    return true;
+  }
+
+  getZonesByZoneIdentifierClientCertificatesIdentifier = async ({ client_certificate_identifier, zone_identifier }: IGetZonesByZoneIdentifierClientCertificatesIdentifierParams): Promise<IGetZonesByZoneIdentifierClientCertificatesIdentifierResponse> => {
+    this.httpService.get(this.url + `/zones/${zone_identifier}/client_certificates/${client_certificate_identifier}`)
+    return true;
+  }
+
+  patchZonesByZoneIdentifierClientCertificatesIdentifier = async ({ client_certificate_identifier, zone_identifier }: IPatchZonesByZoneIdentifierClientCertificatesIdentifierParams): Promise<IPatchZonesByZoneIdentifierClientCertificatesIdentifierResponse> => {
+    this.httpService.get(this.url + `/zones/${zone_identifier}/client_certificates/${client_certificate_identifier}`)
+    return true;
+  }
+
+  getZonesByZoneIdApiGatewayOperations = async ({   zone_id, direction, endpoint, feature, host, method, order, page, per_page }: IGetZonesByZoneIdApiGatewayOperationsParams): Promise<IGetZonesByZoneIdApiGatewayOperationsResponse> => {
+    this.httpService.get(this.url + `/zones/${zone_id}/api_gateway/operations`)
+    return true;
+  }
+
+  postZonesByZoneIdApiGatewayOperations = async ({   zone_id,  endpoint,  host, method   }: IPostZonesByZoneIdApiGatewayOperationsParams): Promise<IPostZonesByZoneIdApiGatewayOperationsResponse> => {
+    this.httpService.post(this.url + `/zones/${zone_id}/api_gateway/operations`)
+    return true;
+  }
+
+  deleteZonesByZoneIdApiGatewayOperationsOperationsId = async ({   zone_id,  operation_id   }: IDeleteZonesByZoneIdApiGatewayOperationsOperationsIdParams): Promise<IDeleteZonesByZoneIdApiGatewayOperationsOperationsIdResponse> => {
+    this.httpService.delete(this.url + `/zones/${zone_id}/api_gateway/operations/${operation_id}`)
+    return true;
+  }
+
+  getZonesByZoneIdApiGatewayOperationsOperationsId = async ({   zone_id,  operation_id, feature  }: IGetZonesByZoneIdApiGatewayOperationsOperationsIdParams): Promise<IGetZonesByZoneIdApiGatewayOperationsOperationsIdResponse> => {
+    this.httpService.get(this.url + `/zones/${zone_id}/api_gateway/operations/${operation_id}`)
+    return true;
+  }
+
+  getZonesByZoneIdApiGatewaySchemas = async ({   zone_id,  feature, host  }: IGetZonesByZoneIdApiGatewaySchemasParams): Promise<IGetZonesByZoneIdApiGatewaySchemasResponse> => {
+    this.httpService.get(this.url + `/zones/${zone_id}/api_gateway/schemas`)
+    return true;
+  }
+
+  patchZonesByZoneIdApiGatewayOperationsSchemaValidation = async ({   zone_id  }: IPatchZonesByZoneIdApiGatewayOperationsSchemaValidationParams): Promise<IPatchZonesByZoneIdApiGatewayOperationsSchemaValidationResponse> => {
+    this.httpService.patch(this.url + `/zones/${zone_id}/api_gateway/operations/schema_validation`)
+    return true;
+  }
+
+  getZonesByZoneIdApiGatewayOperationsOperationIdSchemaValidation = async ({   zone_id, operation_id  }: IGetZonesByZoneIdApiGatewayOperationsOperationIdSchemaValidationParams): Promise<IGetZonesByZoneIdApiGatewayOperationsOperationIdSchemaValidationResponse> => {
+    this.httpService.get(this.url + `/zones/${zone_id}/api_gateway/operations/${operation_id}/schema_validation`)
+    return true;
+  }
+
+  putZonesByZoneIdApiGatewayOperationsOperationIdSchemaValidation = async ({   zone_id, operation_id  }: IPutZonesByZoneIdApiGatewayOperationsOperationIdSchemaValidationParams): Promise<IPutZonesByZoneIdApiGatewayOperationsOperationIdSchemaValidationResponse> => {
+    this.httpService.put(this.url + `/zones/${zone_id}/api_gateway/operations/${operation_id}/schema_validation`)
+    return true;
+  }
+
+  getZonesByZoneIdApiGatewaySettingsSchemaValidation = async ({   zone_id }: IGetZonesByZoneIdApiGatewaySettingsSchemaValidationParams): Promise<IGetZonesByZoneIdApiGatewaySettingsSchemaValidationResponse> => {
+    this.httpService.get(this.url + `/zones/${zone_id}/api_gateway/settings/schema_validation`)
+    return true;
+  }
+
+  patchZonesByZoneIdApiGatewaySettingsSchemaValidation = async ({   zone_id, validation_default_mitigation_action , validation_override_mitigation_action }: IPatchZonesByZoneIdApiGatewaySettingsSchemaValidationParams): Promise<IPatchZonesByZoneIdApiGatewaySettingsSchemaValidationResponse> => {
+    this.httpService.patch(this.url + `/zones/${zone_id}/api_gateway/settings/schema_validation`)
+    return true;
+  }
+
+  putZonesByZoneIdApiGatewaySettingsSchemaValidation = async ({   zone_id, validation_default_mitigation_action , validation_override_mitigation_action }: IPutZonesByZoneIdApiGatewaySettingsSchemaValidationParams): Promise<IPutZonesByZoneIdApiGatewaySettingsSchemaValidationResponse> => {
+    this.httpService.put(this.url + `/zones/${zone_id}/api_gateway/settings/schema_validation`)
+    return true;
+  }
+
+  getZonesByZoneIdApiGatewayUserSchemas = async ({   zone_id, omit_source, page, per_page, validation_enabled }: IGetZonesByZoneIdApiGatewayUserSchemasParams): Promise<IGetZonesByZoneIdApiGatewayUserSchemasResponse> => {
+    this.httpService.get(this.url + `/zones/${zone_id}/api_gateway/user_schemas`)
+    return true;
+  }
+
+  postZonesByZoneIdApiGatewayUserSchemas = async ({   zone_id, file, kind, name, validation_enabled }: IPostZonesByZoneIdApiGatewayUserSchemasParams): Promise<IPostZonesByZoneIdApiGatewayUserSchemasResponse> => {
+    this.httpService.post(this.url + `/zones/${zone_id}/api_gateway/user_schemas`)
+    return true;
+  }
+
+  deleteZonesByZoneIdApiGatewayUserSchemasSchemaId = async ({   zone_id, schema_id }: IDeleteZonesByZoneIdApiGatewayUserSchemasSchemaIdParams): Promise<IDeleteZonesByZoneIdApiGatewayUserSchemasSchemaIdResponse> => {
+    this.httpService.delete(this.url + `/zones/${zone_id}/api_gateway/user_schemas/${schema_id}`)
+    return true;
+  }
+
+  getZonesByZoneIdApiGatewayUserSchemasSchemaId = async ({   zone_id, schema_id , omit_source}: IGetZonesByZoneIdApiGatewayUserSchemasSchemaIdParams): Promise<IGetZonesByZoneIdApiGatewayUserSchemasSchemaIdResponse> => {
+    this.httpService.get(this.url + `/zones/${zone_id}/api_gateway/user_schemas/${schema_id}`)
+    return true;
+  }
+
+  patchZonesByZoneIdApiGatewayUserSchemasSchemaId = async ({   zone_id, schema_id , validation_enabled}: IPatchZonesByZoneIdApiGatewayUserSchemasSchemaIdParams): Promise<IPatchZonesByZoneIdApiGatewayUserSchemasSchemaIdResponse> => {
+    this.httpService.patch(this.url + `/zones/${zone_id}/api_gateway/user_schemas/${schema_id}`)
+    return true;
+  }
+
+  getZonesByZoneIdApiGatewayUserSchemasSchemaIdOperations = async ({   zone_id, schema_id ,   endpoint, feature, host, method, operation_status, page, per_page  }: IGetZonesByZoneIdApiGatewayUserSchemasSchemaIdOperationsParams): Promise<IGetZonesByZoneIdApiGatewayUserSchemasSchemaIdOperationsResponse> => {
+    this.httpService.get(this.url + `/zones/${zone_id}/api_gateway/user_schemas/${schema_id}/operations`)
+    return true;
+  }
+
+  //API Shields Settings Start
+
+
+
+
+
+
+
+
+
+
+
 }
--- lib/module/index.d.ts
+++ lib/module/index.d.ts
@@ -106,3 +106,610 @@ export interface IPatchZonesByZoneIdApiGatewayDiscoveryOperationsByOperationIdRe
     success: boolean,
   }
 }
+
+//Burası
+
+export interface IGetZonesByZoneIdentifierCertificateAuthoritiesHostnameAssociationsParams {
+  zone_identifier: string,
+  mtls_certificate_id?: string,
+}
+
+export interface IGetZonesByZoneIdentifierCertificateAuthoritiesHostnameAssociationsResponse {
+  data: {
+    result: {
+      hostnames: Array<string>,
+      mtls_certificate_id: string,
+    },
+    errors: Array<{
+      code: number,
+      message: string,
+    }>,
+    messages: Array<{
+      code: number,
+      message: string,
+    }>,
+    success: boolean,
+  }
+}
+
+export interface IPutZonesByZoneIdentifierCertificateAuthoritiesHostnameAssociationsParams {
+  zone_identifier: string,
+  hostnames?: Array<string>,
+  mtls_certificate_id?: string,
+}
+
+export interface IPutZonesByZoneIdentifierCertificateAuthoritiesHostnameAssociationsResponse {
+  data: {
+    result: {
+      hostnames: Array<string>,
+      mtls_certificate_id: string,
+    },
+    errors: Array<{
+      code: number,
+      message: string,
+    }>,
+    messages: Array<{
+      code: number,
+      message: string,
+    }>,
+    success: boolean,
+  }
+}
+
+export interface IGetZonesByZoneIdentifierClientCertificatesParams {
+  zone_identifier: string,
+  limit?: number,
+  offset?: number,
+  page?: number,
+  per_page?: number,
+  status?: boolean, // buna bak
+}
+
+export interface IGetZonesByZoneIdentifierClientCertificatesResponse {
+  data: {
+    result: Array<object>,
+    errors: Array<{
+      code: number,
+      message: string,
+    }>,
+    messages: Array<{
+      code: number,
+      message: string,
+    }>,
+    success: boolean,
+    result_info?: {
+      count?: number,
+      page?: number,
+      per_page?: number,
+      total_count?: number,
+    }
+  }
+}
+
+export interface IPostZonesByZoneIdentifierClientCertificatesParams {
+  zone_identifier: string,
+  csr: string,
+  validity_days: number,
+}
+
+export interface IPostZonesByZoneIdentifierClientCertificatesResponse {
+  data: {
+    result: {
+      certificate: string,
+      certificate_authority: object,
+      common_name: string,
+      country: string,
+      csr: string,
+      expires_on: string,
+      fingerprint_sha256: string,
+      id: string,
+      issued_on: string,
+      location: string,
+      organization: string,
+      organizational_unit: string,
+      serial_number: string,
+      signature: string,
+      ski: string,
+      state: string,
+      status: boolean, //buraya bak
+      validity_days: number,
+    },
+    errors: Array<{
+      code: number,
+      message: string,
+    }>,
+    messages: Array<{
+      code: number,
+      message: string,
+    }>,
+    success: boolean,
+  }
+}
+
+export interface IDeleteZonesByZoneIdentifierClientCertificatesIdentifierParams {
+  client_certificate_identifier: string,
+  zone_identifier: string,
+}
+
+export interface IDeleteZonesByZoneIdentifierClientCertificatesIdentifierResponse {
+  data: {
+    result: {
+      certificate: string,
+      certificate_authority: object,
+      common_name: string,
+      country: string,
+      csr: string,
+      expires_on: string,
+      fingerprint_sha256: string,
+      id: string,
+      issued_on: string,
+      location: string,
+      organization: string,
+      organizational_unit: string,
+      serial_number: string,
+      signature: string,
+      ski: string,
+      state: string,
+      status: boolean, //buraya bak
+      validity_days: number,
+    },
+    errors: Array<{
+      code: number,
+      message: string,
+    }>,
+    messages: Array<{
+      code: number,
+      message: string,
+    }>,
+    success: boolean,
+  }
+}
+
+export interface IGetZonesByZoneIdentifierClientCertificatesIdentifierParams {
+  client_certificate_identifier: string,
+  zone_identifier: string,
+}
+
+export interface IGetZonesByZoneIdentifierClientCertificatesIdentifierResponse {
+  data: {
+    result: {
+      certificate: string,
+      certificate_authority: object,
+      common_name: string,
+      country: string,
+      csr: string,
+      expires_on: string,
+      fingerprint_sha256: string,
+      id: string,
+      issued_on: string,
+      location: string,
+      organization: string,
+      organizational_unit: string,
+      serial_number: string,
+      signature: string,
+      ski: string,
+      state: string,
+      status: boolean, //buraya bak
+      validity_days: number,
+    },
+    errors: Array<{
+      code: number,
+      message: string,
+    }>,
+    messages: Array<{
+      code: number,
+      message: string,
+    }>,
+    success: boolean,
+  }
+}
+
+export interface IPatchZonesByZoneIdentifierClientCertificatesIdentifierParams {
+  client_certificate_identifier: string,
+  zone_identifier: string,
+}
+
+export interface IPatchZonesByZoneIdentifierClientCertificatesIdentifierResponse {
+  data: {
+    result: {
+      certificate: string,
+      certificate_authority: object,
+      common_name: string,
+      country: string,
+      csr: string,
+      expires_on: string,
+      fingerprint_sha256: string,
+      id: string,
+      issued_on: string,
+      location: string,
+      organization: string,
+      organizational_unit: string,
+      serial_number: string,
+      signature: string,
+      ski: string,
+      state: string,
+      status: boolean, //buraya bak
+      validity_days: number,
+    },
+    errors: Array<{
+      code: number,
+      message: string,
+    }>,
+    messages: Array<{
+      code: number,
+      message: string,
+    }>,
+    success: boolean,
+  }
+}
+
+export interface IGetZonesByZoneIdApiGatewayOperationsParams {
+  zone_id: string,
+  direction?: string,
+  endpoint?: string,
+  feature?: Array<string>,
+  host?: Array<string>,
+  method?: Array<string>,
+  order?: string,
+  page?: number,
+  per_page?: number,
+}
+
+export interface IGetZonesByZoneIdApiGatewayOperationsResponse {
+  data: {
+    result: Array<object>,
+
+    errors: Array<{
+      code: number,
+      message: string,
+    }>,
+    messages: Array<{
+      code: number,
+      message: string,
+    }>,
+    success: boolean,
+  }
+}
+
+export interface IPostZonesByZoneIdApiGatewayOperationsParams {
+  zone_id: string,
+  endpoint: string,
+  host: string
+  method: string,
+}
+
+export interface IPostZonesByZoneIdApiGatewayOperationsResponse {
+  data: {
+    result: Array<object>,
+
+    errors: Array<{
+      code: number,
+      message: string,
+    }>,
+    messages: Array<{
+      code: number,
+      message: string,
+    }>,
+    success: boolean,
+    result_info?: {
+      count: number,
+      page: number,
+      per_page: number,
+      total_count: number,
+    }
+  }
+}
+export interface IDeleteZonesByZoneIdApiGatewayOperationsOperationsIdParams {
+  operation_id: string,
+  zone_id: string,
+}
+
+export interface IDeleteZonesByZoneIdApiGatewayOperationsOperationsIdResponse {
+  data: {
+
+    errors: Array<{
+      code: number,
+      message: string,
+    }>,
+    messages: Array<{
+      code: number,
+      message: string,
+    }>,
+    success: boolean,
+    result: object,
+  }
+}
+
+export interface IGetZonesByZoneIdApiGatewayOperationsOperationsIdParams {
+  operation_id: string,
+  zone_id: string,
+  feature?: Array<string>,
+}
+
+export interface IGetZonesByZoneIdApiGatewayOperationsOperationsIdResponse {
+  data: {
+    result: {
+      endpoint: string,
+      host: string,
+      method: string,
+      features: {
+        last_updated: string,
+        operation_id: string,
+      }
+    },
+    errors: Array<{
+      code: number,
+      message: string,
+    }>,
+    messages: Array<{
+      code: number,
+      message: string,
+    }>,
+    success: boolean,
+  }
+}
+
+export interface IGetZonesByZoneIdApiGatewaySchemasParams {
+  zone_id: string,
+  feature?: Array<string>,
+  host?: Array<string>,
+}
+
+export interface IGetZonesByZoneIdApiGatewaySchemasResponse {
+  data: {
+    result: {
+      schemas: Array<object>,
+      timestamp: string,
+    },
+    errors: Array<{
+      code: number,
+      message: string,
+    }>,
+    messages: Array<{
+      code: number,
+      message: string,
+    }>,
+    success: boolean,
+  }
+}
+
+export interface IPatchZonesByZoneIdApiGatewayOperationsSchemaValidationParams {
+  zone_id: string,
+}
+
+export interface IPatchZonesByZoneIdApiGatewayOperationsSchemaValidationResponse {
+  data: {
+    result: object,
+    errors: Array<{
+      code: number,
+      message: string,
+    }>,
+    messages: Array<{
+      code: number,
+      message: string,
+    }>,
+    success: boolean,
+  }
+}
+export interface IGetZonesByZoneIdApiGatewayOperationsOperationIdSchemaValidationParams {
+  operation_id: string,
+  zone_id: string,
+}
+
+export interface IGetZonesByZoneIdApiGatewayOperationsOperationIdSchemaValidationResponse {
+  data: {
+    mitigation_action: string,
+  }
+}
+
+export interface IPutZonesByZoneIdApiGatewayOperationsOperationIdSchemaValidationParams {
+  operation_id: string,
+  zone_id: string,
+  mitigation_action?: string,
+}
+
+export interface IPutZonesByZoneIdApiGatewayOperationsOperationIdSchemaValidationResponse {
+  data: {
+    mitigation_action: string,
+  }
+}
+
+export interface IGetZonesByZoneIdApiGatewaySettingsSchemaValidationParams {
+  zone_id: string,
+}
+
+export interface IGetZonesByZoneIdApiGatewaySettingsSchemaValidationResponse {
+  data: {
+    validation_default_mitigation_action: string,
+    validation_override_mitigation_action: string,
+  }
+}
+
+export interface IPatchZonesByZoneIdApiGatewaySettingsSchemaValidationParams {
+  zone_id: string,
+  validation_default_mitigation_action?: string,
+  validation_override_mitigation_action?: string,
+}
+
+export interface IPatchZonesByZoneIdApiGatewaySettingsSchemaValidationResponse {
+  data: {
+    validation_default_mitigation_action: string,
+    validation_override_mitigation_action: string,
+  }
+}
+
+export interface IPutZonesByZoneIdApiGatewaySettingsSchemaValidationParams {
+  zone_id: string,
+  validation_default_mitigation_action?: string,
+  validation_override_mitigation_action?: string,
+}
+
+export interface IPutZonesByZoneIdApiGatewaySettingsSchemaValidationResponse {
+  data: {
+    validation_default_mitigation_action: string,
+    validation_override_mitigation_action: string,
+  }
+}
+export interface IGetZonesByZoneIdApiGatewayUserSchemasParams {
+  zone_id: string,
+  omit_source?: boolean,
+  page?: number,
+  per_page?: number,
+  validation_enabled?: boolean,
+}
+
+export interface IGetZonesByZoneIdApiGatewayUserSchemasResponse {
+  data: {
+    result: Array<object>,
+
+    errors: Array<{
+      code: number,
+      message: string,
+    }>,
+    messages: Array<{
+      code: number,
+      message: string,
+    }>,
+    success: boolean,
+    result_info?: {
+      count: number,
+      page: number,
+      per_page: number,
+      total_count: number,
+    }
+  }
+}
+export interface IPostZonesByZoneIdApiGatewayUserSchemasParams {
+  zone_id: string,
+  file: string,
+  kind: string,
+  name?: string,
+  validation_enabled?: string,
+}
+
+export interface IPostZonesByZoneIdApiGatewayUserSchemasResponse {
+  data: {
+    result: {
+      schemas: object,
+      upload_details: object,
+    },
+    errors: Array<{
+      code: number,
+      message: string,
+    }>,
+    messages: Array<{
+      code: number,
+      message: string,
+    }>,
+    success: boolean,
+  }
+}
+export interface IDeleteZonesByZoneIdApiGatewayUserSchemasSchemaIdParams {
+  schema_id: string,
+  zone_id: string,
+}
+
+export interface IDeleteZonesByZoneIdApiGatewayUserSchemasSchemaIdResponse {
+  data: {
+    errors: Array<{
+      code: number,
+      message: string,
+    }>,
+    messages: Array<{
+      code: number,
+      message: string,
+    }>,
+    result: object,
+    success: boolean,
+  }
+}
+export interface IGetZonesByZoneIdApiGatewayUserSchemasSchemaIdParams {
+  schema_id: string,
+  zone_id: string,
+  omit_source?: boolean,
+}
+
+export interface IGetZonesByZoneIdApiGatewayUserSchemasSchemaIdResponse {
+  data: {
+    result: {
+      created_at: string,
+      kind: string,
+      name: string,
+      schema_id: string,
+      source?: string,
+      validation_enabled?: boolean,
+    }
+    errors: Array<{
+      code: number,
+      message: string,
+    }>,
+    messages: Array<{
+      code: number,
+      message: string,
+    }>,
+    
+    success: boolean,
+  }
+}
+export interface IPatchZonesByZoneIdApiGatewayUserSchemasSchemaIdParams {
+  schema_id: string,
+  zone_id: string,
+  validation_enabled?: boolean,
+}
+
+export interface IPatchZonesByZoneIdApiGatewayUserSchemasSchemaIdResponse {
+  data: {
+    result: {
+      created_at: string,
+      kind: string,
+      name: string,
+      schema_id: string,
+      source?: string,
+      validation_enabled?: boolean,
+    }
+    errors: Array<{
+      code: number,
+      message: string,
+    }>,
+    messages: Array<{
+      code: number,
+      message: string,
+    }>,
+    
+    success: boolean,
+  }
+}
+
+export interface IGetZonesByZoneIdApiGatewayUserSchemasSchemaIdOperationsParams {
+  schema_id: string,
+  zone_id: string,
+  endpoint:string, 
+  feature: Array<string>, 
+  host: Array<string>, 
+  method: Array<string>, 
+  operation_status: string, 
+  page?: number, 
+  per_page?: number,
+}
+
+export interface IGetZonesByZoneIdApiGatewayUserSchemasSchemaIdOperationsResponse {
+  data: {
+    result: Array<any>
+    errors: Array<{
+      code: number,
+      message: string,
+    }>,
+    messages: Array<{
+      code: number,
+      message: string,
+    }>,
+    
+    success: boolean,
+    result_info?: {
+      count: number,
+      page: number,
+      per_page: number,
+      total_count: number,
+    }
+  }
+}
